6d19d0e2932791c9f717c680a09a1c98b8a9e7cd
Vincenzo <mr.vincenzo.galante@gmail.com>
2023-02-04 08:30:49 +0000

    adding prefect gh block

---
+++ etl_web_to_gcs_github_prefect.py
@@ -0,0 +1,11 @@
+from prefect.deployments import Deployment
+from prefect.filesystems import GitHub
+from etl_web_to_gcs_github import etl_web_to_gcs  # <-- my ETL script
+
+github_block = GitHub.load("dtc-prefect-gh")
+
+github_deployment = Deployment.build_from_flow(
+    flow=etl_web_to_gcs, name="web-to-gc-bucket-gh", storage=github_block
+)
+
+github_deployment.apply()
