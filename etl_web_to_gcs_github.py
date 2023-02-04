f4507f017063783e3b3c1864c27ee89344c3b3ca
Vincenzo <mr.vincenzo.galante@gmail.com>
2023-02-04 10:40:48 +0000

    adding prefect decorators

--- etl_web_to_gcs_github.py
+++ etl_web_to_gcs_github.py
@@ -5,11 +5,13 @@ from prefect import task, flow
 from prefect_gcp.cloud_storage import GcsBucket
 
 
+@task(log_prints=True, name="GET FROM WEB")
 def get_from_web(dataset_url: str) -> bytes:
     raw = requests.get(dataset_url)
     return raw.content
 
 
+@task(log_prints=True, name="TO PARQUET")
 def df_to_parquet(raw: bytes) -> pd.DataFrame:
     df = pd.read_csv(BytesIO(raw), compression="gzip")
     buffer = BytesIO()
@@ -21,21 +23,26 @@ def df_to_parquet(raw: bytes) -> pd.DataFrame:
     return buffer
 
 
-# 2 save as raw
+@task(log_prints=True, name="TO BUCKET")
 def upload_to_bucket(encoded: bytes, storage_url: str) -> None:
     gcs_block = GcsBucket.load("dtc-gcs-bucket")
     gcs_block.upload_from_file_object(encoded, storage_url)
 
 
-# 3 save as paquet to gcbucket
+@flow(log_prints=True)
+def data_to_bucket(color: str, year: int, month: int) -> None:
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{color}_tripdata_{year}-{month:02}.csv.gz"
+    storage_url = f"{color}_taxi_data/{color}_tripdata_{year}-{month:02}.parquet"
+
+    raw = get_from_web(dataset_url)
+    parquet = df_to_parquet(raw)
+    upload_to_bucket(parquet, storage_url)
 
 
 if __name__ == "__main__":
-
     color = "green"
     year = 2020
     month = 11
-
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{color}_tripdata_{year}-{month:02}.csv.gz"
     storage_url = f"{color}_taxi_data/{color}_tripdata_{year}-{month:02}.parquet"
 
--- etl_web_to_gcs_github_prefect.py
+++ etl_web_to_gcs_github_prefect.py
@@ -1,11 +1,11 @@
 from prefect.deployments import Deployment
 from prefect.filesystems import GitHub
-from etl_web_to_gcs_github import etl_web_to_gcs  # <-- my ETL script
+from etl_web_to_gcs_github import data_to_bucket  # <-- my ETL script
 
 github_block = GitHub.load("dtc-prefect-gh")
 
 github_deployment = Deployment.build_from_flow(
-    flow=etl_web_to_gcs,
+    flow=data_to_bucket,
     name="web-to-gc-bucket-gh",
     storage=github_block,
     parameters={"color": "green", "year": 2020, "months": [11]},
